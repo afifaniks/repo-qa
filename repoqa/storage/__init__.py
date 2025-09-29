c9fc34f3b4bb6007ea7ddac056fd286c1fdbc414
Afif Al Mamun <afifanik@gmail.com>
2025-09-29 15:54:15 -0600

    Add SPDX license header

--- repoqa/storage/__init__.py
+++ repoqa/storage/__init__.py
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2025 Afif Al Mamun
+
 from repoqa.storage.chroma_store import ChromaVectorStore
 from repoqa.storage.vector_store import VectorStore
 
--- repoqa/storage/chroma_store.py
+++ repoqa/storage/chroma_store.py
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2025 Afif Al Mamun
+
 import uuid
 from typing import Any, Dict, List, Optional, Sequence
 
