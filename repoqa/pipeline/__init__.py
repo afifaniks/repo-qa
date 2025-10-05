b16f84e1952ba85cdfb06f086e85477507703e24
Afif Al Mamun <afifanik@gmail.com>
2025-10-05 18:07:52 -0400

    Update init

--- repoqa/pipeline/__init__.py
+++ repoqa/pipeline/__init__.py
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: MIT
 # Copyright (c) 2025 Afif Al Mamun
 
-from repoqa.pipeline.langchain_rag import LangChainRAGPipeline
+from repoqa.pipeline.pipeline import Pipeline
 from repoqa.pipeline.rag import RAGPipeline
 
-__all__ = ["RAGPipeline", "LangChainRAGPipeline"]
+__all__ = ["Pipeline", "RAGPipeline"]
